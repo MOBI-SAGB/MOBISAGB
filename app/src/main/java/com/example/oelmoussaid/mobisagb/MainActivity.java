d55445a1dbbd59c21a1227efc934dd528a3ecc51
crOwSid <housni.saad@gmail.com>
2016-11-07 11:02:45 +0000

    New main activity

--- app/src/main/java/com/example/oelmoussaid/mobisagb/MainActivity.java
+++ app/src/main/java/com/example/oelmoussaid/mobisagb/MainActivity.java
@@ -12,15 +12,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-    Button btnInsert;
-    EditText txtNom;
-    EditText txtPass;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -103,12 +97,5 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
-    }
-
-    public void initIHM(){
-
-        btnInsert=(Button) findViewById(R.id.btnInsert);
-        txtNom=(EditText) findViewById(R.id.txtName);
-        txtPass=(EditText) findViewById(R.id.txtPass);
     }
 }
